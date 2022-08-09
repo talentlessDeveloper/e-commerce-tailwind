9506fbf3b08ee7d43cf3105d33d6516b3d8018d4
chimoski <chimaobiprince867@gmail.com>
2022-08-09 22:52:36 +0100

    sanity installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import {Home} from './pages/Home';
 import {Login} from './pages/Login';
-import SignUp from './pages/SignUp';
+import  SignUp from './pages/SignUp';
 import  Layout  from './pages/Layout';
 
 function App() {
