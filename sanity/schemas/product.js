dcb4fb2ce051dd9aca4c5eb03809655b0a862fdc
chimoski <chimaobiprince867@gmail.com>
2022-08-11 21:48:48 +0100

    setting up schema

--- sanity/schemas/product.js
+++ sanity/schemas/product.js
@@ -3,83 +3,56 @@ export default {
   title: 'Product',
   type: 'document',
   fields: [
-    {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-    },
-    {
-      title: 'Default variant',
-      name: 'defaultProductVariant',
-      type: 'productVariant',
-    },
-    {
-      title: 'Variants',
-      name: 'variants',
-      type: 'array',
-      of: [
-        {
-          title: 'Variant',
-          type: 'productVariant',
-        },
-      ],
-    },
-    {
-      title: 'Tags',
-      name: 'tags',
-      type: 'array',
-      of: [
-        {
-          type: 'string',
-        },
-      ],
-      options: {
-        layout: 'tags',
-      },
-    },
-    {
-      name: 'vendor',
-      title: 'Vendor',
-      type: 'reference',
-      to: {type: 'vendor'},
-    },
-    {
-      name: 'blurb',
-      title: 'Blurb',
-      type: 'localeString',
-    },
-    {
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: {type: 'category'},
-        },
-      ],
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'localeBlockContent',
-    },
-  ],
 
-  preview: {
-    select: {
-      title: 'title',
-      manufactor: 'manufactor.title',
-      media: 'defaultProductVariant.images[0]',
-    },
-  },
+             {
+                name: 'category',
+                title: 'Category',
+                type: 'string',
+             },
+                {
+                    name: 'image',
+                    title: 'Image',
+                    type: 'array',
+                    of:[{type: 'image'}],
+                    options: {
+                        hotspot: true,
+                    }
+                },
+                {
+                    name: 'relatedProducts',
+                    title: 'Related Products',
+                    type: 'array',
+                    of:[{type: 'object'}],
+                    options: {
+                        hotspot: true,
+                    }
+                },
+                {
+                    name: 'name',
+                    title: 'Name',
+                    type: 'string'
+                },
+                {
+                    name: 'slug',
+                    title: 'Slug',
+                    type: 'slug',
+                    option: {
+                        source: 'name',
+                        maxLength: 96,
+                    }
+                },
+                {
+                    name: 'price',
+                    title: 'Price',
+                    type: 'number'
+          
+                },
+                {
+                    name: 'details',
+                    title: 'Details',
+                    type: 'string',
+                }
+          
+            ],
+
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { client } from '../lib/sanitySetup'
+import { fetchAllProductsquery } from '../lib/query'
 
 export const Home = () => {
+  useEffect(()=>{
+    client.fetch(fetchAllProductsquery)
+    .then(data => console.log(data))
+  });
+  
   return (
     <>
     <main>
